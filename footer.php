82aee91ecc54ce5c473f950672347458d59ecb42
Shengyu Zhu <ucapszh@ucl.ac.uk>
2024-11-28 13:12:55 +0000

    diy footer

--- footer.php
+++ footer.php
@@ -1,6 +1,43 @@
 <!-- If you like, you can put a page footer (something that should show up at
      the bottom of every page, such as helpful links, layout, etc.) here. -->
+<!-- Footer Section -->
+<footer class="footer bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <!-- Footer Links -->
+    <div class="row">
+     <!-- About us 关于我们 -->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">About Us</h5>
+        <p>
+          We are an online auction platform committed to providing the best auction experience for our users. Feel free to browse our listings or create your own auctions.
+        </p>
+      </div>
+     <!-- Quick links 快速链接 -->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Quick Links</h5>
+        <ul class="list-unstyled mb-0">
+          <li><a href="index.php" class="text-dark">Home</a></li>
+          <li><a href="register.php" class="text-dark">Register</a></li>
+        </ul>
+      </div>
+     <!-- Contact us 联系我们 -->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Contact Us</h5>
+        <ul class="list-unstyled mb-0">
+          <li><i class="fas fa-map-marker-alt"></i> University College London, Gower Street, London, WC1E 6BT</li>
+          <li><i class="fas fa-phone"></i> Tel: +44 (0) 20 7679 2000</li>
+        </ul>
+      </div>
+    </div>
+    <!-- Footer Links -->
+  </div>
 
+  <!-- Copyright 版权 -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2024 Auction Platform: All rights reserved.
+  </div>
+  <!-- Copyright -->
+</footer>
 
 
 <!-- Bootstrap core JavaScript -->
